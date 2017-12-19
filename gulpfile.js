b5b2715784d1b3fc35d89c60496b41a49c5edc5b
Pozharov Roman <ru.roman.ps@gmail.com>
2017-12-19 07:09:03 +0400

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     server = require('browser-sync'),
+    reload = server.reload,
     sass = require('gulp-sass'),
     minify = require('gulp-csso'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
-    imagemin = require('gulp-tinypng');
+    tinypng = require('gulp-tinypng');
     
 
 gulp.task('styles', function() {
   gulp.src('sass/style.scss')
   .pipe(plumber())
   .pipe(sass())
-  .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+  .pipe(autoprefixer({ browsers: ['last 4 versions'] }))
   .pipe(gulp.dest('css'))
 //  .pipe(minify())
 //  .pipe(rename('style.min.css'))
 //  .pipe(gulp.dest('css'))
-  .pipe(server.reload({stream: true}));
+  .pipe(reload({stream: true}));
 });
 
-gulp.task('serve', function(){
+gulp.task('webserver', function(){
   server.init({
     server: {
       baseDir: './'
@@ -31,11 +32,11 @@ gulp.task('serve', function(){
 
 gulp.task('tinypng', function() {
   gulp.src('img/*')
-  .pipe(imagemin('OqvQ0pn0iAaJbR8QeJbvbVf5t41GplS'))
+  .pipe(tinypng('OqvQ0pn0iAaJbR8QeJbvbVf5t41GplS'))
   .pipe(gulp.dest('img'));
 });
 
-gulp.task ('watch', ['serve', 'styles'], function() {
+gulp.task ('watch', ['webserver', 'styles'], function() {
   gulp.watch('sass/**/*.scss', ['styles']);
   gulp.watch('*.html', server.reload);
   gulp.watch('js/*.js', server.reload);
